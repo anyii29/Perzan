51602d859460e7d9680b95aa8b3514692bdbef45
Angeles <gonzalez.angeles19@gmail.com>
2015-11-03 19:38:04 -0600

    Se subieron archivos del juego

---
+++ 10_JUEGO/Nueva carpeta/healthy run/Assets/scripts/controladorPersonaje.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ControladorPersonaje : MonoBehaviour {
+	
+	public float fuerzaSalto = 100f;
+	
+	private bool enSuelo = true;
+	public Transform comprobadorSuelo;
+	private float comprobadorRadio = 0.07f;
+	public LayerMask mascaraSuelo;
+	
+	private bool dobleSalto = false;
+	
+	private Animator animator;
+	
+	void Awake(){
+		animator = GetComponent<Animator>();
+	}
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	void FixedUpdate(){
+		enSuelo = Physics2D.OverlapCircle(comprobadorSuelo.position, comprobadorRadio, mascaraSuelo);
+		animator.SetBool("isGrounded", enSuelo);
+		if(enSuelo){
+			dobleSalto = false;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if((enSuelo || !dobleSalto) && Input.GetMouseButtonDown(0)){
+			GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, fuerzaSalto);
+			//rigidbody2D.AddForce(new Vector2(0, fuerzaSalto));
+			if(!dobleSalto && !enSuelo){
+				dobleSalto = true;
+			}
+		}
+	}
+}

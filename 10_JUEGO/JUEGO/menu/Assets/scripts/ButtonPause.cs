be66fee0b6cd1193c62b83578aac1e900806e247
Angeles <gonzalez.angeles19@gmail.com>
2015-11-06 00:22:26 -0600

    se subio el menu del juego, checar si funciona, principalmente el de pausa... y me falto por ahi creo algo pero se necesita anclar con el juego completo me parace

---
+++ 10_JUEGO/JUEGO/menu/Assets/scripts/ButtonPause.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPause : MonoBehaviour {
+	private bool paused;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update() {
+	}
+	public void PauseGame()
+	{
+		paused = !paused;
+		if (paused == true) {
+			paused = false;
+		}
+		if (paused == false) {
+			paused = true;
+		}
+
+	}
+}
